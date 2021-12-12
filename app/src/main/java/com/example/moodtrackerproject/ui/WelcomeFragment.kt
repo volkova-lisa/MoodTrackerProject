94ce8664bec312fc5e03a47f46fef1db1013dc0d
volkova-lisa <liza.volk999@gmail.com>
2021-12-12 23:19:42 +0200

    splash screen via launch theme

--- app/src/main/java/com/example/moodtrackerproject/ui/WelcomeFragment.kt
+++ app/src/main/java/com/example/moodtrackerproject/ui/WelcomeFragment.kt
@@ -4,21 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.example.moodtrackerproject.R
 import com.example.moodtrackerproject.databinding.FragmentWelcomeScreenBinding
 import com.example.moodtrackerproject.ui.login.LoginFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class WelcomeFragment : Fragment() {
 
     private var _binding: FragmentWelcomeScreenBinding? = null
     private val mBinding get() = _binding!!
+    lateinit var navBar: BottomNavigationView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentWelcomeScreenBinding.inflate(layoutInflater, container, false)
+
+        navBar = requireActivity()!!.findViewById(R.id.bottom_navigation)
+        navBar.isVisible = false
 
         mBinding.welcomeButton.setOnClickListener {
             val transaction = requireActivity().supportFragmentManager.beginTransaction()
