e91cda0d369ce1fc2fe20026eab677490c612b89
volkova-lisa <liza.volk999@gmail.com>
2021-12-13 17:34:47 +0200

    Some addictive fixes

--- app/src/main/java/com/example/moodtrackerproject/SplashActivity.kt
+++ app/src/main/java/com/example/moodtrackerproject/SplashActivity.kt
@@ -1,15 +1,14 @@
 package com.example.moodtrackerproject
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.moodtrackerproject.routing.Route
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        Route.goToMainActivity(this)
         finish()
     }
 }
--- app/src/main/java/com/example/moodtrackerproject/routing/Route.kt
+++ app/src/main/java/com/example/moodtrackerproject/routing/Route.kt
@@ -1,6 +1,9 @@
 package com.example.moodtrackerproject.routing
 
+import android.content.Context
+import android.content.Intent
 import androidx.fragment.app.FragmentActivity
+import com.example.moodtrackerproject.MainActivity
 import com.example.moodtrackerproject.R
 import com.example.moodtrackerproject.ui.LoginFragment
 
@@ -9,5 +12,9 @@ object Route {
         val transaction = fragmentActivity.supportFragmentManager.beginTransaction()
         transaction.replace(R.id.nav_host_fragment, LoginFragment())
         transaction.commit()
+    }
+    fun goToMainActivity(context: Context) {
+        var intent = Intent(context, MainActivity::class.java)
+        context.startActivity(intent)
     }
 }
