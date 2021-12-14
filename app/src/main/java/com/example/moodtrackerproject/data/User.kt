d028b581c21470af631ab3d4d2f74418ff745c3b
volkova-lisa <liza.volk999@gmail.com>
2021-12-14 13:23:26 +0200

    Separate user data from ui step 1

--- app/src/main/java/com/example/moodtrackerproject/data/User.kt
+++ app/src/main/java/com/example/moodtrackerproject/data/User.kt
@@ -1,4 +1,7 @@
 package com.example.moodtrackerproject.data
 
-data class User {
+data class User(val email: String, val name: String, val password: String) {
+    override fun toString(): String {
+        return "$email - $password - $name"
+    }
 }
---
+++ app/src/main/java/com/example/moodtrackerproject/data/UserDao.kt
@@ -0,0 +1,5 @@
+package com.example.moodtrackerproject.data
+
+class UserDao {
+
+}
