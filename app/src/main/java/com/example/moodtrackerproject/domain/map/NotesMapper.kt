150d0b969af94eff30f67d5f1eae8e8cf4b08b98
volkova-lisa <liza.volk999@gmail.com>
2022-01-10 12:57:55 +0200

    Some fixes for mapper

--- app/src/main/java/com/example/moodtrackerproject/domain/map/NotesMapper.kt
+++ app/src/main/java/com/example/moodtrackerproject/domain/map/NotesMapper.kt
@@ -2,15 +2,22 @@ package com.example.moodtrackerproject.domain.map
 
 import com.example.moodtrackerproject.domain.NoteBody
 import com.example.moodtrackerproject.ui.notes.list.NoteBodyUiModel
+import timber.log.Timber
 
 class NotesMapper : Mapper<NoteBody, NoteBodyUiModel> {
     private fun mapFrom(date: String, title: String, text: String) = NoteBodyUiModel().also {
-        it.noteId = ""
+        it.noteId = System.currentTimeMillis().toString()
+        it.date = date
         it.title = title
         it.text = text
+        it.isChecked = false
+        //TODO("change")
+        it.checkChanged = {changed -> Timber.d(changed)}
+        it.openDetails = {open -> Timber.d(open)}
+
     }
 
-    override fun map(from: NoteBody): NoteBodyUiModel {
-        TODO("Not yet implemented")
+    override fun map(from: NoteBody) = NoteBodyUiModel().apply {
+        //TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/moodtrackerproject/ui/notes/list/NoteBodyUiModel.kt
+++ app/src/main/java/com/example/moodtrackerproject/ui/notes/list/NoteBodyUiModel.kt
@@ -1,13 +1,15 @@
 package com.example.moodtrackerproject.ui.notes.list
 
+import timber.log.Timber
+
 data class NoteBodyUiModel(
     var noteId: String = "", // text.hashCode()//UUID.randomUUID().toString(), //System.currentTimeMillis(),"",
 
-    val date: String = "",
+    var date: String = "",
     var title: String = "",
     var text: String = " ",
-    val isChecked: Boolean = false,
+    var isChecked: Boolean = false,
 
-    val checkChanged: (String) -> Unit,
-    val openDetails: (String) -> Unit,
+    var checkChanged: (String) -> Unit = { string -> Timber.d("HELLO") },
+    var openDetails: (String) -> Unit = { string -> Timber.d("HELLO") },
 )
