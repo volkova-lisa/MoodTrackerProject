e78f7700f286721f35ac3e8f8da817726cc1a0ae
volkova-lisa <liza.volk999@gmail.com>
2022-01-11 23:49:52 +0200

    Several ui fixes

--- app/src/main/java/com/example/moodtrackerproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/moodtrackerproject/ui/home/HomeFragment.kt
@@ -1,12 +1,11 @@
 package com.example.moodtrackerproject.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.moodtrackerproject.MainActivity
+import com.example.moodtrackerproject.R
 import com.example.moodtrackerproject.databinding.FragmentHomeBinding
 import com.example.moodtrackerproject.utils.PreferenceManager
 
@@ -22,18 +21,19 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
-        binding.run {
-            logout.setOnClickListener {
-                viewModel.logOut()
-                PreferenceManager.setInitUser(false)
-            }
-        }
+//        binding.run {
+//            logout.setOnClickListener {
+//                viewModel.logOut()
+//                PreferenceManager.setInitUser(false)
+//            }
+//        }
 
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
         viewModel.liveData.observe(viewLifecycleOwner, {
             render(it)
         })
@@ -41,6 +41,19 @@ class HomeFragment : Fragment() {
 
     private fun render(state: HomeViewState) {
         state.action?.let { handleAction(it) }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logOut -> {
+                viewModel.logOut()
+                PreferenceManager.setInitUser(false)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun handleAction(homeAction: HomeAction) {
