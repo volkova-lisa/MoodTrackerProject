e426edb000f5636263a815c932602ebf7fde5745
Alexey Kabanets <alexey.kabanets@sigma.software>
2022-05-23 19:28:06 +0300

    Add ResetPassword stubs

---
+++ app/src/main/java/com/example/moodtrackerproject/ui/reset_password/ResetPasswordViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.moodtrackerproject.ui.reset_password
+
+import com.example.moodtrackerproject.ui.BaseViewModel
+
+class ResetPasswordViewModel : BaseViewModel<ResetPasswordProps>()
