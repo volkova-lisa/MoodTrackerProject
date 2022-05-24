de8f1653cdf2c36ec5a05d8e4c836c7afa4a1bdd
Alexey Kabanets <alexey.kabanets@sigma.software>
2022-05-24 22:47:20 +0300

    Refactor Test Results

--- app/src/main/java/com/example/moodtrackerproject/app/AppState.kt
+++ app/src/main/java/com/example/moodtrackerproject/app/AppState.kt
@@ -5,6 +5,7 @@ import com.example.moodtrackerproject.app.mood.MoodState
 import com.example.moodtrackerproject.app.notes.NoteDetailsState
 import com.example.moodtrackerproject.app.notes.NotesState
 import com.example.moodtrackerproject.app.tests.StressTestState
+import com.example.moodtrackerproject.app.tests.TestResultsState
 
 data class AppState(
     // FIXME: login/registration/reset_password must also have states, but we'll skip them for simplicity
@@ -12,5 +13,6 @@ data class AppState(
     val noteDetailsState: NoteDetailsState = NoteDetailsState(),
     val moodState: MoodState = MoodState(),
     val addMoodState: AddMoodState = AddMoodState(),
-    val stressTestState: StressTestState = StressTestState()
+    val stressTestState: StressTestState = StressTestState(),
+    val testResultsState: TestResultsState = TestResultsState(),
 )
--- app/src/main/java/com/example/moodtrackerproject/app/Store.kt
+++ app/src/main/java/com/example/moodtrackerproject/app/Store.kt
@@ -5,6 +5,7 @@ import com.example.moodtrackerproject.app.mood.MoodState
 import com.example.moodtrackerproject.app.notes.NoteDetailsState
 import com.example.moodtrackerproject.app.notes.NotesState
 import com.example.moodtrackerproject.app.tests.StressTestState
+import com.example.moodtrackerproject.app.tests.TestResultsState
 
 object Store {
     var appState = AppState()
@@ -27,5 +28,9 @@ object Store {
 
     fun setState(newState: MoodState) {
         appState = appState.copy(moodState = newState)
+    }
+
+    fun setState(newState: TestResultsState) {
+        appState = appState.copy(testResultsState = newState)
     }
 }
