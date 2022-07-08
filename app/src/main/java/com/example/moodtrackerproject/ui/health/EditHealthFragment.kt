54bca8a01839c9a2d81a5a6686cf07fe459f92a1
volkova-lisa <liza.volk999@gmail.com>
2022-07-08 18:33:26 +0100

    Health edit part of UI created

---
+++ app/src/main/java/com/example/moodtrackerproject/ui/health/EditHealthFragment.kt
@@ -0,0 +1,18 @@
+package com.example.moodtrackerproject.ui.health
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.moodtrackerproject.R
+class EditHealthFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_health, container, false)
+    }
+}
