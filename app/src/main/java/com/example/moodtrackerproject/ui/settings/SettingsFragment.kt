62ef9dee628dd3942c49abc6f2caf348876d892e
volkova-lisa <liza.volk999@gmail.com>
2022-07-27 00:59:36 +0100

    Only god knows how it works, some sort of random sh**

--- app/src/main/java/com/example/moodtrackerproject/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/moodtrackerproject/ui/settings/SettingsFragment.kt
@@ -1,10 +1,10 @@
 package com.example.moodtrackerproject.ui.settings
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.moodtrackerproject.R
 import com.example.moodtrackerproject.databinding.FragmentSettingsBinding
 import com.example.moodtrackerproject.ui.BaseFragment
 import com.yariksoffice.lingver.Lingver
@@ -23,30 +23,45 @@ class SettingsFragment : BaseFragment<SettingsViewModel, FragmentSettingsBinding
 
     override fun onResume() {
         super.onResume()
-        Log.d("onResume -------", resources.configuration.locale.toString())
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("before -------", resources.configuration.locale.toString())
         binding?.run {
             langSwitch.setOnCheckedChangeListener { buttonView, onSwitch ->
+//                val sharedPreferences = (requireActivity() as MainActivity).getSharedPreferences("Settings", Activity.MODE_PRIVATE)
+//                val language = sharedPreferences.getString("My_Lang", "") ?: "es"
+//                Lingver.getInstance().setLocale(requireContext(), language)
+//                Log.d("pref -------=", language.toString())
+
                 if (onSwitch) {
                     Lingver.getInstance().setLocale(requireContext(), "en")
+                    enTitle.setTextColor(resources.getColor(R.color.light_purple))
+                    uaTitle.setTextColor(resources.getColor(R.color.text_grey))
+//                    val editor = (requireActivity() as MainActivity).getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+//                    editor.putString("My_Lang", "en")
+//                    editor.apply()
+//                    (requireActivity() as MainActivity).recreate()
+
                     // fragmentManager.beginTransaction().replace(R.id.settingsFragment, fragm).commitAllowingStateLoss()
-                } else Lingver.getInstance().setLocale(requireContext(), "ua")
+                } else {
+                    Lingver.getInstance().setLocale(requireContext(), "uk-rUA")
+                    uaTitle.setTextColor(resources.getColor(R.color.light_purple))
+                    enTitle.setTextColor(resources.getColor(R.color.text_grey))
+//                    val editor = (requireActivity() as MainActivity).getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+//                    editor.putString("My_Lang", "uk-rUA")
+//                    editor.apply()
+//                    (requireActivity() as MainActivity).recreate()
+                }
+                // Log.d("pref -------", language.toString())
             }
-            Log.d("after -------", resources.configuration.locale.toString())
         }
     }
 
     override fun render(props: SettingsProps) {
-        Log.d("before loadLocate-------", resources.configuration.locale.toString())
-
         binding?.run {
             langSwitch.setOnCheckedChangeListener { buttonView, onSwitch ->
             }
-            Log.d("after in render -------", resources.configuration.locale.toString())
         }
     }
 }
