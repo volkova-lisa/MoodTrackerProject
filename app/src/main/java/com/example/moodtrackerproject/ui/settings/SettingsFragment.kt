2d3cb7d5499166d7005281571545b223b0e83030
volkova-lisa <liza.volk999@gmail.com>
2022-07-27 21:02:36 +0100

    On Half way on adding dark theme

--- app/src/main/java/com/example/moodtrackerproject/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/moodtrackerproject/ui/settings/SettingsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.moodtrackerproject.R
 import com.example.moodtrackerproject.databinding.FragmentSettingsBinding
 import com.example.moodtrackerproject.ui.BaseFragment
@@ -21,39 +22,26 @@ class SettingsFragment : BaseFragment<SettingsViewModel, FragmentSettingsBinding
         container: ViewGroup?
     ): FragmentSettingsBinding = FragmentSettingsBinding.inflate(inflater, container, false)
 
-    override fun onResume() {
-        super.onResume()
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.run {
             langSwitch.setOnCheckedChangeListener { buttonView, onSwitch ->
-//                val sharedPreferences = (requireActivity() as MainActivity).getSharedPreferences("Settings", Activity.MODE_PRIVATE)
-//                val language = sharedPreferences.getString("My_Lang", "") ?: "es"
-//                Lingver.getInstance().setLocale(requireContext(), language)
-//                Log.d("pref -------=", language.toString())
-
                 if (onSwitch) {
                     Lingver.getInstance().setLocale(requireContext(), "en")
                     enTitle.setTextColor(resources.getColor(R.color.light_purple))
                     uaTitle.setTextColor(resources.getColor(R.color.text_grey))
-//                    val editor = (requireActivity() as MainActivity).getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
-//                    editor.putString("My_Lang", "en")
-//                    editor.apply()
-//                    (requireActivity() as MainActivity).recreate()
-
-                    // fragmentManager.beginTransaction().replace(R.id.settingsFragment, fragm).commitAllowingStateLoss()
                 } else {
                     Lingver.getInstance().setLocale(requireContext(), "uk-rUA")
                     uaTitle.setTextColor(resources.getColor(R.color.light_purple))
                     enTitle.setTextColor(resources.getColor(R.color.text_grey))
-//                    val editor = (requireActivity() as MainActivity).getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
-//                    editor.putString("My_Lang", "uk-rUA")
-//                    editor.apply()
-//                    (requireActivity() as MainActivity).recreate()
                 }
-                // Log.d("pref -------", language.toString())
+            }
+            darkModeSwitch.setOnCheckedChangeListener { buttonView, onSwitch ->
+                if (onSwitch) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
             }
         }
     }
